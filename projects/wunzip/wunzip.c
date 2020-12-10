18990a3165b556610178446335938eeb9e1f8f7d
Conner Crosby <crosbyco3@gmail.com>
2020-12-09 22:48:48 -0500

    Finish wunzip by passing all tests

--- projects/wunzip/wunzip.c
+++ projects/wunzip/wunzip.c
@@ -10,15 +10,20 @@ int main (int argc, char *argv[]){
         return 1;
     }
 
-    char buff[2048];
+    int buff[1];
     for(int i = 1; i < argc; i++){
         FILE *fp = fopen(argv[i], "r");
-        while(fread(buff, sizeof(int), 4, fp) != 0 /* and fread(buff, sizeof(char), 4, fp) != 0?????? 4 bytes are int with 1 byte being a single ASCII char */){
-            // for(int j = 0; buff[j] != '\0'; j++){
-            //     printf("%c", buff[j]);
-            // }
-            printf("foo");
-        };
+        while(1){
+            size_t return_code = fread(buff, sizeof(int), 1, fp);
+            if(return_code == 0){
+                break;
+            }
+            int times_to_print = buff[0];
+            fread(buff, sizeof(char), 1, fp);
+            for(int j = 0; j < times_to_print; j++){
+                printf("%c", buff[0]);
+            }
+        }
         fclose(fp); 
     }  
     return 0;
